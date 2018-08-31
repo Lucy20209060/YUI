73fd7506b67a21a63faf8ce62b3f355cc497804d
Lucy20209060 <550502661@qq.com>
2018-09-01 01:14:20 +0800

    chore:基础代码

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+import { Button } from './components'
+
+export default class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Button />
+      </div>
+    );
+  }
+}
---
+++ src/components/button/button.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './button.css';
+
+export default class Button extends React.Component {
+  public render() {
+    return (
+      <div>hahah</div>
+    );
+  }
+}
